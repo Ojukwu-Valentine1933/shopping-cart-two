468231cf37016691bc756ba51b7a1513c9927b09
Ojukwu-Valentine1933 <thejasperval@gmail.com>
2024-03-29 17:51:47 +0100

    "Added CSS styles for favorite button and updated index.html with new structure"
    The code diff includes changes to the index.html file and the addition of CSS styles for the favorite button. The HTML structure has been updated with new div elements for product details and total display. A new div has been added with a placeholder text for the total display. The CSS styles for the favorite button include border, background, cursor, and fill properties for the heart icon, as well as transition and hover effects.

---
+++ color-folder/color.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    let colors = {color_one: "red", color_two: "orange"}
+  
+ var colorBox = document.getElementById("color-box").style.backgroundColor = colors.color_one;
+ var colorButton = document.getElementById("Change-color-button").addEventListener("click", function () {
+    
+ });
+function getRandomColor() {
+   
+    if (colorBox.style.backgroundColor = "black") {
+        colorBox.style.backgroundColor = colors[0];
+    }
+    
+  
+    
+}
+
+//  console.log("DOM content loaded");
+//   document.getElementById("demo").innerText = "DOM content loaded!";
+
+});
